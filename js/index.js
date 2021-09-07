c877534a6109e71dda2614bf4c80ed0ce7f2df92
jeanetchichury <jean.etchichury@hotmail.com>
2021-09-07 17:18:04 -0300

    beta

--- js/index.js
+++ js/index.js
@@ -1,14 +1,35 @@
-let dia = $('#dia')
+let diaAtual = moment().format('YYYY-MM-DD')
+let dia = $('#dia').val(diaAtual)
+let btn = $('button')
+mudaFoto()
 
-
-$.ajax({
-    url: `https://api.nasa.gov/planetary/apod?api_key=mu2JlrbiqzSXIeRvffHEMko8cUPg8GPoIuH6h0xd&date=2021-07-05`,
-    
-    success: function(req){
-        $('body').css('background-image', `url(${req.url})`)
-    },
-
-    error: ()=>{
-      $('p').html('Sinto muito, no dia do seu aniversário não recebemos nenhuma foto da NASA')  
-    }
+btn.on('click', function(event){
+    event.preventDefault()
+    let diaPedido = dia.val()
+    mudaFoto()
 })
+
+function mudaFoto(){
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=mu2JlrbiqzSXIeRvffHEMko8cUPg8GPoIuH6h0xd&date=${dia.val()}`,
+        
+        success: function(req){
+            if (req.media_type == 'image'){
+                $('body').css('background-image', `url(${req.url})`)
+                $('#frame').remove()
+                $('#erro').html('')
+            }
+            else
+            $('#video').html(`<iframe id="frame" src="${req.url}" allowfullscreen>`)
+            $('#erro').html('')
+            $('h2').html(req.title)
+            $('#descritor').html(req.explanation)
+        },
+
+        error: ()=>{
+        $('#erro').html('Sinto muito, no dia do seu aniversário não recebemos nenhuma foto da NASA')
+        $('h2').html("")
+        $('#descritor').html("")
+        }
+    })
+}
